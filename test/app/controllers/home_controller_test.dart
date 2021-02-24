9bd68673f217b2cfd4c967777c91b51edfb72120
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-24 19:33:11 -0300

    test: ensure HomeController converts from United States Dollar to Brazilian Real

--- test/app/controllers/home_controller_test.dart
+++ test/app/controllers/home_controller_test.dart
@@ -1,4 +1,5 @@
 import 'package:currency_converter/app/controllers/home_controller.dart';
+import 'package:currency_converter/app/models/currency_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -16,5 +17,29 @@ main() {
     homeController.convert();
 
     expect(result.text, '0.36');
+  });
+
+  test('should convert from United States Dolar to Brazilian Real', () {
+    from.text = '1.0';
+
+    homeController.fromCurrency = CurrencyModel(
+      name: 'United States Dollar',
+      brazilianReal: 5.43,
+      unitedStatesDollar: 1.0,
+      euro: 0.82,
+      bitcoin: 0.000020,
+    );
+
+    homeController.toCurrency = CurrencyModel(
+      name: 'Brazilian Real',
+      brazilianReal: 1.0,
+      unitedStatesDollar: 0.18,
+      euro: 0.15,
+      bitcoin: 0.0000037,
+    );
+
+    homeController.convert();
+
+    expect(result.text, '5.43');
   });
 }
