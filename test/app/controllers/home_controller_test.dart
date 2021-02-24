56073bf63f9a59c61736c33b955240fc199b707b
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-24 19:34:20 -0300

    test: mock HomeController tests

--- test/app/controllers/home_controller_test.dart
+++ test/app/controllers/home_controller_test.dart
@@ -14,6 +14,22 @@ main() {
   test('should convert from Brazilian Real to United States Dollar', () {
     from.text = '2.0';
 
+    homeController.fromCurrency = CurrencyModel(
+      name: 'Brazilian Real',
+      brazilianReal: 1.0,
+      unitedStatesDollar: 0.18,
+      euro: 0.15,
+      bitcoin: 0.0000037,
+    );
+
+    homeController.toCurrency = CurrencyModel(
+      name: 'United States Dollar',
+      brazilianReal: 5.43,
+      unitedStatesDollar: 1.0,
+      euro: 0.82,
+      bitcoin: 0.000020,
+    );
+
     homeController.convert();
 
     expect(result.text, '0.36');
